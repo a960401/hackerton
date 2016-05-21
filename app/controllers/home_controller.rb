ac80080ce5180088ae2dd9717245b038c9121a17
a960401 <a960401sogang@gmail.com>
2016-05-21 05:47:56 +0000

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
   end
+  def apply
+    
+  end
+  
+  def complete
+    
+  end
+
+  def confirm
+    
+  end
+
+  def matching
+    
+  end
+  
+  def reserve_list
+    
+  end
+
+
 end
